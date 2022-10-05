22dee41ed609de1a766577947cfa4a10c7f1da53
Lee Han Bin <65403285+habi39@users.noreply.github.com>
2022-10-05 21:47:09 +0800

    Updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 // FileName: index.js
 // Import express
+import * as dotenv from 'dotenv';
+dotenv.config()
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
@@ -33,6 +35,7 @@ router.delete('/contacts/:contact_id',deleteContact)
 let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
 if(mongoDB == undefined){
+     console.log(process.env.ENV)
   mongoose.connect("mongodb://localhost:27017/mydb", { useNewUrlParser: true , useUnifiedTopology: true});
 } else{
   mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
